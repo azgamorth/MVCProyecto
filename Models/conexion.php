92b4d6e762a6971123decefbac954fbab5f2e631
azgamorth <necdaniel@hotmail.com>
2020-05-14 10:38:58 -0500

    Cambio en datos de la base de datos servidor nube

---
+++ Models/conexion.php
@@ -0,0 +1,16 @@
+    <?php
+
+//Se definen los parametros para la conexion a la base de datos
+    class Conectar{
+
+            public static function conecta(){
+
+                //$conecta = new mysqli("localhost","root","","trabajo");
+                $conecta = new mysqli("remotemysql.com:3306","sMTJoiJ0p1","jU4YYjF3ws","sMTJoiJ0p1");
+                $conecta ->query("SET NAMES 'utf-8'");
+                return $conecta;    
+            }
+
+    }
+
+?>
