8b2dc72800dff50ef4e419895b9343a252ea5a8e
azgamorth <necdaniel@hotmail.com>
2020-05-14 11:07:23 -0500

    cargue index

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 //ruta por la cual encuentra el metodo que lo lleva a las vista donde se ejecuta la consulta
     require_once "Controllers/usuarios_controllers.php";
@@ -11,18 +10,4 @@
 
 
 
-=======
-<?php
-//ruta por la cual encuentra el metodo que lo lleva a las vista donde se ejecuta la consulta
-    require_once "Controllers/usuarios_controllers.php";
-
-
-
-
-
-
-
-
-
->>>>>>> bf393ce1c66991c4ff5070bdaff1dc6e70b216cd
 ?>
